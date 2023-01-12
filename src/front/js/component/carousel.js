bc318e769c74160a558338728e039b42d06c1823
frako23 <frako23@gmail.com>
2023-01-12 10:51:35 +0000

    ajustes en landing

---
+++ src/front/js/component/carousel.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "../../styles/index.css";
+
+export const Carousel = () => {
+  return (
+    <div
+      id="carousel"
+      className="container-fluid carousel slide overflow-hidden"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img
+            src="https://i.postimg.cc/7LyfYbgM/BANNER3.png"
+            className="d-block w-100"
+            alt="..."
+          ></img>
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://i.postimg.cc/zDdbSzr8/BANNER2.png"
+            className="d-block w-100"
+            alt="..."
+          ></img>
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://i.postimg.cc/m2JPJmkn/BANNER.png"
+            className="d-block w-100"
+            alt="..."
+          ></img>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carousel"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Anterior</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carousel"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Siguiente</span>
+      </button>
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+    </div>
+  );
+};
