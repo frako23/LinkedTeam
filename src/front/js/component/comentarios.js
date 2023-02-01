b6d8d23f3e4778b4bbb6d9531494576a32c80ba4
danielmoret <dmoret17@gmail.com>
2023-02-01 19:41:32 +0000

    cambios en el useEffect en la vista Video

--- src/front/js/component/comentarios.js
+++ src/front/js/component/comentarios.js
@@ -5,26 +5,25 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 export const Comentarios = () => {
   const [comentarios, setComentarios] = useState([]);
 
-  useEffect(() => {
+  async function getComments() {
     try {
-      const fetchData = async () => {
-        const resp = await fetch(
-          `https://jsonplaceholder.typicode.com/comments`
-        );
+      const resp = await fetch(`https://jsonplaceholder.typicode.com/comments`);
 
-        console.log("Aqui2");
-        if (!resp.ok) {
-          let danger = await resp.json();
-          alert(danger);
-        }
-        const data = await resp.json();
-        setComentarios(data);
-      };
-      console.log("Aqui");
-      fetchData();
+      if (!resp.ok) {
+        let danger = await resp.json();
+        alert(danger);
+        return;
+      }
+
+      const data = await resp.json();
+      setComentarios(data);
     } catch (error) {
-      console.error("There has been an error fecth");
+      console.error(error);
     }
+  }
+
+  useEffect(() => {
+    getComments();
   }, []);
 
   return (
@@ -57,7 +56,7 @@ export const Comentarios = () => {
 
       {comentarios.map((comentario) => {
         return (
-          <div className="comentario-publicado">
+          <div className="comentario-publicado" key={comentario.id}>
             <div className="comentario-content">
               <div className="img-user">
                 <img className="profile-img" src={rigoImageUrl} />
