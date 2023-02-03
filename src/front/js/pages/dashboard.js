d1348e7f26f7081784bbf44527469cce2aeade89
frako23 <frako23@gmail.com>
2023-02-03 18:14:21 +0000

    cambios en dashboard

--- src/front/js/pages/dashboard.js
+++ src/front/js/pages/dashboard.js
@@ -13,9 +13,30 @@ export const Dashboard = () => {
 
       <Navbar />
 
-      <div className="container">
+      <div className="">
         <div className="kanban-head">
           <strong className="kanban-head-title">CRM</strong>
+        </div>
+        <div className="ms-2 d-flex position-relative">
+          <button type="button" class="btn btn-success me-5 ">
+            <strong>Nuevo Negocio</strong>
+          </button>
+          <div className="texto">
+            <table
+              class="table table-bordered text-white position-absolute top-0 end-0"
+              style={{
+                width: 600,
+              }}
+            >
+              <thead>
+                <tr>
+                  <th scope="col">En Calle= $20.054</th>
+                  <th scope="col">Logrado= $15.024/75%</th>
+                  <th scope="col">Negocios= 15</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
         </div>
 
         <div className="kanban-table">
@@ -50,32 +71,56 @@ export const Dashboard = () => {
               onclick="crearTarea(event)"
             />
           </div>
-
+          {/* columnas */}
           <div
-            className="kanban-block"
-            id="pendientes"
+            className="kanban-block tabla"
+            // id="pendientes"
             ondrop="drop(event)"
             ondragover="allowDrop(event)"
           >
-            <strong>PENDIENTES</strong>
+            <strong>Prospectos</strong>
           </div>
 
           <div
-            className="kanban-block"
-            id="procesos"
+            className="kanban-block tabla"
+            // id="procesos"
             ondrop="drop(event)"
             ondragover="allowDrop(event)"
           >
-            <strong>EN PROCESO</strong>
+            <strong>Prospecto Calficado</strong>
           </div>
 
           <div
-            className="kanban-block"
-            id="completados"
+            className="kanban-block tabla"
+            // id="completados"
             ondrop="drop(event)"
             ondragover="allowDrop(event)"
           >
-            <strong>COMPLETADOS</strong>
+            <strong>Contacto realizado</strong>
+          </div>
+          <div
+            className="kanban-block tabla"
+            // id="completados"
+            ondrop="drop(event)"
+            ondragover="allowDrop(event)"
+          >
+            <strong>Primera Cita</strong>
+          </div>
+          <div
+            className="kanban-block tabla"
+            // id="completados"
+            ondrop="drop(event)"
+            ondragover="allowDrop(event)"
+          >
+            <strong>Negociación Iniciada</strong>
+          </div>
+          <div
+            className="kanban-block tabla"
+            // id="compltaetados"
+            ondrop="drop(event)"
+            ondragover="allowDrop(event)"
+          >
+            <strong>Venta Concretada</strong>
           </div>
         </div>
       </div>
