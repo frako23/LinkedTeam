e75d6bb57c9748ee4bfed0140633d8be0ad0e643
frako23 <frako23@gmail.com>
2023-06-23 16:42:05 -0400

    se acomoda la seccion perfil

--- src/front/js/component/userPannel.js
+++ src/front/js/component/userPannel.js
@@ -22,10 +22,12 @@ export const UserPannel = () => {
   return (
     <main
       className="d-grid"
-      style={{
-        paddingLeft: "9rem",
-        paddingRight: "6rem",
-      }}
+      style={
+        {
+          // paddingLeft: "9rem",
+          // paddingRight: "6rem",
+        }
+      }
     >
       <span className="badge bg-info text-dark">
         {actions.calcularDiasDeUso(store.usuario.created_at)} dias de uso
@@ -60,9 +62,58 @@ export const UserPannel = () => {
         </p>
       </div>
       <div className="perfil-dashboard">
-        <Box />
+        <section className="about">
+          <div className="image">
+            <img src={mujer} className="" />
+          </div>
+          <div className="content">
+            <h3>Que puedes hacer aqui?</h3>
+            <h5>Fórmate</h5>
+            <p>
+              En CURSOS Y VIDEOS encontrarás contenido de alto valor para seguir
+              mejorando de forma profesional y persona
+            </p>
 
-        <div className="main-wrapper-intro">
+            <h5>Anota tus pendientes</h5>
+            <p>
+              En TAREAS PENDIENTES podrás anotar esas actividades claves, y
+              hacerles seguimiento con las 3 columnas
+            </p>
+            <h5>Tu propio CRM</h5>
+            <p>
+              Con esta APP podrás guardar cada uno de tus prospectos, y usar el
+              embudo de ventas para hacer un seguimiento efectivo
+            </p>
+
+            <h5>Registra tus actividades</h5>
+            <p>
+              Al registrar un cliente verás el boton DETALLE con el que podrás
+              agregar todas las activiades que haz realizado
+            </p>
+            <h5>Asi esta tu embudo de ventas</h5>
+            {/* <div className="icons-container">
+              <div className="icons">
+                <i className="fas fa-user"></i>
+                <span>adsfadfadfsdfsd</span>
+              </div>
+              <div className="icons">
+                <i className="fas fa-user"></i>
+                <span>adsfadfadfsdfsd</span>
+              </div>
+              <div className="icons">
+                <i className="fas fa-user"></i>
+                <span>adsfadfadfsdfsd</span>
+              </div>
+              <div className="icons">
+                <i className="fas fa-user"></i>
+                <span>adsfadfadfsdfsd</span>
+              </div>
+            </div> */}
+            <Box />
+          </div>
+        </section>
+
+        {/*<div className="main-wrapper-intro">
           <div className="wrapper-intro">
             <div className="intro-square one-intro">
               <h5>Fórmate</h5>
@@ -93,9 +144,7 @@ export const UserPannel = () => {
               </p>
             </div>
           </div>
-        </div>
-
-        <img src={mujer} className="img-intro" />
+        </div> */}
       </div>
       {/* <ToDoChart /> */}
     </main>
