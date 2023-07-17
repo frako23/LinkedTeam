225446d4052a73f8a89c0119bde97fe3c3fb060e
frako23 <frako23@gmail.com>
2023-07-17 09:24:54 -0400

    se corrigen los estilos de comentarios y repuestas

--- src/front/js/component/comentarios.js
+++ src/front/js/component/comentarios.js
@@ -1,62 +1,63 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { Context } from '../store/appContext'
-import '../../styles/comments.css'
-import rigoImageUrl from '../../img/rigo-baby.jpg'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/comments.css";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { useParams } from "react-router-dom";
 
 export const Comentarios = () => {
-  const [comentario, setComentario] = useState('')
-  const [responder, setResponder] = useState(false)
-  const [commentSelected, setCommentSelected] = useState(null)
-  const [respuestas, setRespuestas] = useState({}) // Estado de las respuestas como objeto
-  const { store, actions } = useContext(Context)
-  const { theid } = useParams()
-  console.log(theid)
+  const [comentario, setComentario] = useState("");
+  const [responder, setResponder] = useState(false);
+  const [commentSelected, setCommentSelected] = useState(null);
+  const [respuestas, setRespuestas] = useState({}); // Estado de las respuestas como objeto
+  const { store, actions } = useContext(Context);
+  const { theid } = useParams();
+  console.log(theid);
 
   const handleRespuestaChange = (event) => {
-    setRespuestas({ [event.target.id]: event.target.value })
-  }
+    setRespuestas({ [event.target.id]: event.target.value });
+  };
 
   const getRespuestas = (id) => {
     if (commentSelected == id) {
-      setCommentSelected(null)
+      setCommentSelected(null);
     } else {
-      setCommentSelected(id)
+      setCommentSelected(id);
     }
-    actions.getRespuestas(id)
-  }
+    actions.getRespuestas(id);
+  };
 
   useEffect(() => {
-    console.log(theid)
-    actions.getComentarios(theid)
-  }, [])
+    console.log(theid);
+    actions.getComentarios(theid);
+  }, []);
 
   return (
     <>
       <form
         onSubmit={(e) => {
-          e.preventDefault()
+          e.preventDefault();
           actions.postComentarios(
             {
-              content: comentario
+              content: comentario,
             },
             theid
-          )
-          setComentario('')
-          console.log('entro aqui', comentario)
+          );
+          setComentario("");
+          console.log("entro aqui", comentario);
         }}
       >
         <div className="container-comentarios">
-          <h4 className="comment-header">
+          {/* <h4 className="comment-header">
             Escribe aquí cualquier duda o comentario
-          </h4>
+          </h4> */}
           <div className="new-comentario">
-            <textarea
+            <input
               className="input-comentario"
-              placeholder="Escribe tu pregunta o comentario"
+              placeholder="Escribe aquí cualquier duda o comentario"
               onChange={(e) => setComentario(e.target.value)}
               value={comentario}
-            ></textarea>
+              type="text"
+            ></input>
             <div className="form-group">
               <button type="submit" id="post" className="btn btn-warning">
                 Publicar
@@ -71,47 +72,63 @@ export const Comentarios = () => {
           <div className="comentario-publicado" key={comentario.id}>
             <div
               style={{
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column'
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
               }}
             >
               <div className="comentario-content mb-3 mt-3">
-                <div className="comment">
-                  <div className="comentario">
-                    <p>
-                      <strong>{comentario.name}</strong>
-                    </p>
-                    <p>{comentario.content}</p>
+                <div className="comentario">
+                  <h3 className="usuario">
+                    <strong>{comentario.name}</strong>
+                  </h3>
+                  <p>{comentario.content}</p>
+                  <div className="comment-action">
+                    <span onClick={() => setResponder(!responder)}>
+                      Responder
+                    </span>
+                    {store.respuestas !== "" ? (
+                      <span onClick={() => getRespuestas(comentario.id)}>
+                        {commentSelected !== comentario.id
+                          ? "Ver respuestas"
+                          : "Ocultar respuestas"}
+                      </span>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 </div>
-                <div className="input-group mb-3 mt-3">
-                  <input
-                    type="text"
-                    id={comentario.id}
-                    className="form-control"
-                    placeholder="Responde este comentario"
-                    aria-label={comentario.id}
-                    aria-describedby={comentario.id}
-                    onChange={(event) => handleRespuestaChange(event)}
-                    value={respuestas[comentario.id] || ''}
-                  />
-                  <div className="input-group-append">
-                    <button
-                      className="btn btn-secondary"
-                      type="button"
-                      onClick={() => {
-                        actions.postRespuestas(
-                          respuestas[comentario.id],
-                          comentario.id
-                        )
-                        setRespuestas({})
-                      }}
-                    >
-                      <i className="fa-solid fa-reply"></i> Responder
-                    </button>
+
+                {responder && (
+                  <div className="input-group mb-3 mt-3">
+                    <input
+                      type="text"
+                      id={comentario.id}
+                      className="form-control respuesta"
+                      placeholder="Responde este comentario"
+                      aria-label={comentario.id}
+                      aria-describedby={comentario.id}
+                      onChange={(event) => handleRespuestaChange(event)}
+                      value={respuestas[comentario.id] || ""}
+                    />
+                    <div className="input-group-append">
+                      <button
+                        className="btn btn-secondary"
+                        type="button"
+                        onClick={() => {
+                          actions.postRespuestas(
+                            respuestas[comentario.id],
+                            comentario.id
+                          );
+                          setRespuestas({});
+                        }}
+                      >
+                        <i className="fa-solid fa-reply"></i> Responder
+                      </button>
+                    </div>
                   </div>
-                </div>
+                )}
+
                 {store.respuestas.map((resp) => {
                   if (
                     resp.comment_id === comentario.id &&
@@ -126,24 +143,14 @@ export const Comentarios = () => {
                           <p>{resp.content}</p>
                         </div>
                       </div>
-                    )
+                    );
                   }
                 })}
-                <button
-                  className="btn btn-warning"
-                  style={{ alignSelf: 'end' }}
-                  onClick={() => getRespuestas(comentario.id)}
-                >
-                  <i className="fa-solid fa-reply"></i>
-                  {commentSelected !== comentario.id
-                    ? 'Ver respuestas'
-                    : 'Ocultar respuestas'}
-                </button>
               </div>
             </div>
           </div>
-        )
+        );
       })}
     </>
-  )
-}
+  );
+};
