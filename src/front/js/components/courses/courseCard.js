ee51bb7f8a6fcfb17817ed129399f6c36b863584
frako23 <frako23@gmail.com>
2024-04-26 14:21:31 -0400

    arregla la sección de cursos disponibles

--- src/front/js/components/courses/courseCard.js
+++ src/front/js/components/courses/courseCard.js
@@ -2,61 +2,34 @@ import React, { useContext, useEffect } from "react";
 import { Context } from "../../store/appContext";
 import "../../../styles/home.css";
 import { Link } from "react-router-dom";
-import img2 from "../../../img/nohayvideos.jpg";
-import img from "../../../img/exitosa-empresaria-trabajando-equipo-portatil-su-oficina-vestida-ropa-blanca.jpg";
 
 export function CourseCard() {
   const { store, actions } = useContext(Context);
-  // console.log(store.usuario.agency);
+
   useEffect(() => {
     actions.getUsuario();
-    // console.log(store.usuario);
   }, []);
 
-  if (store.usuario.agency !== null) {
-    useEffect(() => actions.getCourses(store.usuario.agency_id), []);
-    // console.log(store.courses);
-  }
+  useEffect(() => actions.getCourses(store.usuario.manager_id), []);
 
   return (
-    <>
-      {store.usuario.agency === null || store.usuario.agency === undefined ? (
-        <div className="agency-not-set">
-          <img src={img} />
-          <Link className="btn btn-light" to="/perfil">
-            <h2>Primero debes seleccionar la agencia a la que perteneces</h2>
-          </Link>
-        </div>
-      ) : store.courses.length == 0 ? (
-        <div className="video-not-set">
-          <img src={img2} />
-          <Link to="/perfil">
-            <h2>
-              Tu gerente aún no agrega cursos, dile que es importante esta
-              sección para tu formación
-            </h2>
-          </Link>
-        </div>
-      ) : (
-        <div className="container courses__container">
-          {store.courses.map((data) => {
-            return (
-              <article className="course" key={data.id}>
-                <div>
-                  <img className="course__image" src={data.img_url} />
-                </div>
-                <div className="course__info">
-                  <h4>{data.title}</h4>
-                  <p>{data.description}</p>
-                  <Link to={`/video/${data.id}`} className="course__btn">
-                    Ver curso
-                  </Link>
-                </div>
-              </article>
-            );
-          })}
-        </div>
-      )}
-    </>
+    <div className="container courses__container">
+      {store.courses.map((data) => {
+        return (
+          <article className="course" key={data.id}>
+            <div>
+              <img className="course__image" src={data.img_url} />
+            </div>
+            <div className="course__info">
+              <h4>{data.title}</h4>
+              <p>{data.description}</p>
+              <Link to={`/video/${data.id}`} className="course__btn">
+                Ver curso
+              </Link>
+            </div>
+          </article>
+        );
+      })}
+    </div>
   );
 }
