0cea78ac756fa81ac3cb77acfa7cadbc05c4ce69
frako23 <frako23@gmail.com>
2024-05-24 11:51:09 -0400

    cambio a .api

--- src/app.py
+++ src/app.py
@@ -6,11 +6,11 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from api.utils import APIException, generate_sitemap
-from api.models import db
-from api.routes import api
-from api.admin import setup_admin
-from api.commands import setup_commands
+from .api.utils import APIException, generate_sitemap
+from .api.models import db
+from .api.routes import api
+from .api.admin import setup_admin
+from .api.commands import setup_commands
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
 
